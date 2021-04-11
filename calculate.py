43e570a00d9e927e740020aed6458a56deb7bf62
Eduardo Rosas <lalorosas@outlook.com>
2021-04-10 22:47:42 -0500

    Complete comments
    Apparently I didn't save the file before commiting, duh

--- calculate.py
+++ calculate.py
@@ -9,13 +9,19 @@ def calculate_pi(n):
     for _ in range(n):
         x = random.uniform(0,1)
         y = random.uniform(0,1)
+        # pithagoras theorem, no need to square root
+        # if value is <1 its square root is also <1
+        # same for values >1
         dist_to_origin = x**2 + y**2
         if dist_to_origin < 1:
             inside_circle = inside_circle + 1
         inside_square = inside_square + 1
     
-    # 4 is the calculated ratio between the area of a semi-circle with r = 1
-    # that exists inside a 1*1 square 
+    # 4 is the calculated ratio between the area of a
+    # quarter-circle with r = 1 that exists inside a 1*1 square
+    # eg. pi*r**2/4 / r**2 (area of circle divided by 4
+    # because this represents 1/4 of a circle)
+    # given that equation we can isolate pi which is equal to 4 
     return 4 * inside_circle/inside_square
 
 if __name__ == "__main__":
