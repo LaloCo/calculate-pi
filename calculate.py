6939a7314df175c77228e766427bf9d5f89fa35e
Eduardo Rosas <lalorosas@outlook.com>
2021-04-10 22:41:44 -0500

    Add comments

--- calculate.py
+++ calculate.py
@@ -1,5 +1,8 @@
 import random, math, sys
 
+# being able to get a random value between 0 and 1
+# calculate the value of pi
+
 def calculate_pi(n):
     inside_circle = 0.
     inside_square = 0.
@@ -10,6 +13,9 @@ def calculate_pi(n):
         if dist_to_origin < 1:
             inside_circle = inside_circle + 1
         inside_square = inside_square + 1
+    
+    # 4 is the calculated ratio between the area of a semi-circle with r = 1
+    # that exists inside a 1*1 square 
     return 4 * inside_circle/inside_square
 
 if __name__ == "__main__":
